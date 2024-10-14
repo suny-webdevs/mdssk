617fbc19172457dd4abe21103dc74d01772e306c
suny-webdevs <mdsunyshaikh@gmail.com>
2024-10-14 15:02:52 +0600

    add logo and favicon

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -8,25 +8,32 @@ import {
 } from "react-icons/hi"
 import { GoDotFill } from "react-icons/go"
 import { HiArrowUpRight } from "react-icons/hi2"
+import Image from "next/image"
 
 const Navbar = () => {
   const [menuMouseOver, setMenuMouseOver] = useState(false)
   const [talkMouseOver, setTalkMouseOver] = useState(false)
 
   return (
-    <div className={`${container} flex items-center justify-between py-5 mb-3`}>
+    <div className={`${container} flex items-center justify-between mb-3`}>
       <Link
         href={"/"}
         className="text-3xl text-white font-bold uppercase"
       >
-        mdssk
+        <Image
+          src={"/Logo.png"}
+          alt="Logo"
+          width={1920}
+          height={1080}
+          className="w-[200px] h-[100px]"
+        />
       </Link>
       <div className="flex gap-3">
         <button
           onMouseEnter={() => setTalkMouseOver(true)}
           onMouseLeave={() => setTalkMouseOver(false)}
           type="button"
-          className={`${btnMd} bg-white/10 hover:bg-white/20 text-white transition-all duration-200 flex items-center gap-1`}
+          className={`${btnMd} bg-white/10 hover:bg-white/20 text-white transition-all ease-in-out duration-200 flex items-center gap-1`}
         >
           let&apos;s talk{" "}
           {talkMouseOver ? (
@@ -39,7 +46,7 @@ const Navbar = () => {
           onMouseEnter={() => setMenuMouseOver(true)}
           onMouseLeave={() => setMenuMouseOver(false)}
           type="button"
-          className={`${btnMd} bg-white text-black transition-all duration-200 flex items-center gap-1`}
+          className={`${btnMd} bg-white text-black transition-all ease-in-out duration-200 flex items-center gap-1`}
         >
           menu{" "}
           {menuMouseOver ? (
