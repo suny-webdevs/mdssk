0388afcba129d4cbcfdd27fde7404a1e3d76a832
suny-webdevs <mdsunyshaikh@gmail.com>
2024-10-23 02:05:13 +0600

    fix portfolio styles

--- src/components/shared/Cards/PortFolioCard.jsx
+++ src/components/shared/Cards/PortFolioCard.jsx
@@ -7,8 +7,8 @@ import Button from "../Button";
 const PortFolioCard = ({ image, title, category, visitLink, detailsLink }) => {
   return (
     <div className="group w-full rounded-xl from-[#F761E0] via-[#7613F1] to-[#520E9D] md:bg-gradient-to-r md:p-[.125rem]">
-      <div className="flex flex-col justify-center gap-4 rounded-xl bg-primary p-5">
-        <div className="h-[15rem] w-full overflow-hidden rounded-xl">
+      <div className="flex flex-col justify-center gap-4 rounded-xl border bg-primary p-5 md:border-none">
+        <div className="h-[16rem] w-full overflow-hidden rounded-xl">
           <Image
             src={image}
             alt={title}
@@ -24,12 +24,22 @@ const PortFolioCard = ({ image, title, category, visitLink, detailsLink }) => {
           </div>
           <div className="flex items-center gap-4">
             <Link href={detailsLink}>
-              <Button className={"bg-black/20 text-white backdrop-blur-lg"}>
+              <Button
+                className={
+                  "bg-black/20 text-white backdrop-blur-lg hover:bg-black/50"
+                }
+              >
                 Learn More
               </Button>
             </Link>
             <Link href={visitLink}>
-              <Button>Visit Site</Button>
+              <Button
+                className={
+                  "from-from via-via to-to border border-white bg-gradient-to-bl hover:bg-gradient-to-tr"
+                }
+              >
+                Visit Site
+              </Button>
             </Link>
           </div>
         </div>
